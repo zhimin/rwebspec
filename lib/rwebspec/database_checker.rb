ad319f91591c250dfbc4b7eea70adac476bb4361
Zhimin <zhimin@itest2.com>
2009-11-18 06:44:33 +1000

    support sqlserver

--- lib/rwebspec/database_checker.rb
+++ lib/rwebspec/database_checker.rb
@@ -3,8 +3,8 @@ require 'active_record' # change rwebspec/database
 module RWebSpec
   module DatabaseChecker
 
-	# Example
-	#   connect_to_database mysql_db(:host => "localhost", :database => "lavabuild_local", :user => "root", :password => ""), true
+    # Example
+    #   connect_to_database mysql_db(:host => "localhost", :database => "lavabuild_local", :user => "root", :password => ""), true
     def mysql_db(settings)
       options = {:adapter => "mysql"}
       options.merge!(settings)
@@ -17,14 +17,24 @@ module RWebSpec
     end
 
     def sqlserver_db(settings)
-      settings = {:adapter => "sqlserver"}
-      settings[:username] ||= settings[:user]
+      options = {:adapter => "sqlserver"}
+      options[:username] ||= settings[:user]
       options.merge!(settings)
     end
-	
+
     def sqlserver_db_dbi(options)
+      options[:user] ||= options[:username]
+      options[:username] ||= options[:user]
       conn_str = "DBI:ADO:Provider=SQLOLEDB;Data Source=#{options[:host]};Initial Catalog=#{options[:database]};User ID=\"#{options[:user]}\";password=\"#{options[:password]}\" "
       dbh = DBI.connect(conn_str)
+    end
+
+    def clear_database_connection
+      begin
+        ActiveRecord::Base.remove_connection
+      rescue => e
+        puts "failed o clear database connection: #{e}"
+      end
     end
 
     # Connect to databse, example
@@ -37,6 +47,9 @@ module RWebSpec
         begin
           ActiveRecord::Base.connection
         rescue => e
+          require 'pp'
+          pp db_settings
+          puts "failed to connect: #{e}"
           ActiveRecord::Base.establish_connection(db_settings)
         end
       end
@@ -44,17 +57,17 @@ module RWebSpec
 
     def load_table(table_name)
       begin
-         ActiveRecord::Base.connection    
-      rescue =>e 
-        raise "No database connection setup yet, use connect_to_database() method" 
+        ActiveRecord::Base.connection
+      rescue =>e
+        raise "No database connection setup yet, use connect_to_database() method"
       end
       class_name = table_name.classify
       # define the class, so can use ActiveRecord in
       # such as
       #   Perosn.count.should == 2
-      def_class = "class ::#{class_name} < ActiveRecord::Base; end"    
+      def_class = "class ::#{class_name} < ActiveRecord::Base; end"
       eval def_class
-      return def_class        
+      return def_class
     end
 
   end
