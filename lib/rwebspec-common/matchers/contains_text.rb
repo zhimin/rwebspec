854fc52ea6b2da80b33921f4ce68b4e865f8c43f
Zhimin Zhan <zhiminzhan@gmail.com>
2014-08-12 09:14:20 +1000

    contains text to new matcher format

--- lib/rwebspec-common/matchers/contains_text.rb
+++ lib/rwebspec-common/matchers/contains_text.rb
@@ -23,10 +23,13 @@ class ContainsText
   end
 
   # error message for should_not
-  def negative_failure_message
+  def failure_message_when_negated
     "expected '#{actual_text}' not to contain '#{@expected}', but it did"
   end
 
+  # RSpec 2 compatibility:
+  alias_method :failure_message_for_should, :failure_message
+  alias_method :failure_message_for_should_not, :failure_message_when_negated
 end
 
 
@@ -35,3 +38,6 @@ def contains_text?(expected)
   ContainsText.new(expected)
 end
 alias contains? contains_text?
+alias contains contains_text?
+alias contain? contains_text?
+alias contain contains_text?
